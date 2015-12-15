9b24f62b326f438b473ad83ddf8854ca010d6526
tech-camp <kenji.t.dream@gmail.com>
2015-12-15 21:51:23 +0900

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://radiant-brook-2058.herokuapp.com'} #ローカルなのか要確認
+  config.action_mailer.default_url_options = { host: 'bestudy.herokuapp.com'} #ローカルなのか要確認
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
