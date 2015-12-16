2508fef48ad5cbc9e7a7ea2351d438c70c31e1b2
tech-camp <kenji.t.dream@gmail.com>
2015-12-16 17:06:33 +0900

    db 補修

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214041510) do
+ActiveRecord::Schema.define(version: 20151216075806) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "key",        limit: 255
     t.text     "detail",     limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "lectures", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "season",      limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "users", force: :cascade do |t|
