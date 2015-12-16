1964f9a7a55480e443dc6cbab5656ae682cfeb5f
tech-camp <kenji.t.dream@gmail.com>
2015-12-16 17:57:51 +0900

    投稿機能実装

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,8 +1,18 @@
 class LecturesController < ApplicationController
   def index
-    @name = Lecture.all
+    @lectures = Lecture.all
   end
 
   def new
   end
+
+  def create
+    Lecture.create(name: params[:name], season: params[:season], description: params[:description])
+  end
+
+  private
+  def lecture_params
+    params.permit(:name, :season, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   #lecturesへのroutes
   get '/lectures' => 'lectures#index'
   get 'lectures/new' => 'lectures#new'
+  post 'lectures' => 'lectures#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
