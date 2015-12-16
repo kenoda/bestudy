47c16a85d27b75fac1cf79354cad42f4070b496c
tech-camp <kenji.t.dream@gmail.com>
2015-12-16 15:46:26 +0900

    classes 再び

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   #classesへのroutes
   # root "classes#index"
-  get 'classes' => 'classes#index'
+  get '/classes' => 'classes#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
