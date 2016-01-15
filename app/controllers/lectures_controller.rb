a704b8b2824aae19eb35e1f1b7b1158d0fec6159
tech-camp <kenji.t.dream@gmail.com>
2016-01-15 17:42:25 +0900

    レイアウトver.1を網羅して編集

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,4 +1,18 @@
 class LecturesController < ApplicationController
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # sign_inのときに、group_keyも許可する
+    devise_parameter_sanitizer.for(:sign_in) << :group_key
+    # sign_upのときに、group_keyも許可する
+    devise_parameter_sanitizer.for(:sign_up) << :group_key
+    #account_updateのときに、group_keyも許可する
+    devise_parameter_sanitizer.for(:account_update) << :group_key
+  end
+
+
   def index
     @lectures = Lecture.order('id ASC') #最新順に教室を並べる
   end
